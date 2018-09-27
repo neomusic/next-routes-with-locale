89afd48bb1ee04693a7b1318669ad6f9ba9ce4b3
Francesco Bianchi <f.bianchi@palmabit.com>
2018-09-27 18:33:02 +0200

    feat(): detect language from browser

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import pick from 'lodash.pick'
 import NextLink from 'next/link'
 import NextRouter from 'next/router'
 import Route from './Route'
-import { generateRouteFromObjectName, redirectToLocalizedHome } from './helpers/routeHelper'
+import { generateRouteFromObjectName, redirectToLocalizedHome, detectLocale } from './helpers/routeHelper'
 import MiddlewareManager from './middleware/MiddlewareManager'
 
 export default class Routes {
@@ -127,7 +127,7 @@ export default class Routes {
         req.nextRoute = route
         req.siteUrl = this.siteUrl
         req.getMultilanguageUrls = () => this.getMultilanguageUrls(route, query)
-        
+
         const middleware = MiddlewareManager(route.middlewares, { req, res, route, query })
         middleware((err, data) => {
           if (err) {
@@ -145,8 +145,10 @@ export default class Routes {
         return
       }
 
+
       if (req.url === '/' && this.forceLocale) {
-        redirectToLocalizedHome(res, this.locale)
+        const detectedLocale = detectLocale({ req, routes: this.routes, defaultLocale: this.locale })
+        redirectToLocalizedHome(res, detectedLocale)
         return
       }
 
